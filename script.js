15860a03ac668e754afbf5ca9d0b2bbf40a74ab1
Arun kumar <106857719+ArunKumar023@users.noreply.github.com>
2023-04-28 10:24:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,39 +1,25 @@
-let userDetails = document.querrySelector('form');
-const form = document.querySelector('#myForm');
-const name = document.querySelector('#name');
-const age = document.querySelector('#age');
-const btn = document.querySelector('#btn');
+required.
+const nameInput = document.getElementById('name');
+const ageInput = document.getElementById('age');
+const submitBtn = document.getElementById('btn');
 
-userDetails.addEventListener('onsubmit', promiseResolve);
-form.addEventListener('onsubmit', (event) => {
-	event.preventDefault();
+submitBtn.addEventListener('click', (event) => {
+  event.preventDefault(); 
+  const delayPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (Number(ageInput.value) >= 18) {
+        resolve({ name: nameInput.value, age: Number(ageInput.value) });
+      } else {
+        reject({ name: nameInput.value });
+      }
+    }, 4000);
+  });
 
-	if(name.value === '' || age.value === '') {
-		alert('Please fill out all fiels.');
-		return;
-	}
-
-function promiseResolve(event){
-	return new Promise((resolve, reject) => {
-		event.preventDefault();
-	const age = parseInt(age.value);
-
-	const promise = new Promise((resolve, reject) => {
-		setTimeout(() => {
-			if(document.getElementById('age').value >= 18){
-				resolve(alert(`Welcome, ${document.getElementById('name').value}. You can vote.`));
-			}
-			resolve(alert(`Welcome, ${document.getElementById('name').value}. You aren't old enough.`));
-			if(age >= 18) resolve();
-			reject();
-		}, 4 * 1000);
-	});
-}
-
-	promise.then(() => {
-		alert(`Welcome, ${name.value}. You can vote.`);
-	})
-	.catch(() => {
-		alert(`Welcome, ${name.value}. You aren't old enough.`);
-	});
+  delayPromise
+    .then((data) => {
+      alert(`Welcome, ${data.name}. You can vote.`);
+    })
+    .catch((error) => {
+      alert(`Oh sorry ${error.name}. You aren't old enough.`);
+    });
 });
